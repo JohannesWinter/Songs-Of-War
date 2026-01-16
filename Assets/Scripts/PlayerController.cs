4d9e65510cc97c1c0b2262a6f5bcae099e43dfab
JohannesWinter <jvf.winter@web.de>
2026-01-16 15:56:25 +0100

    SortedTryHold_AddedComments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 using static UnityEngine.Rendering.DebugUI;
+using static UnityEngine.UI.Image;
 
 public class PlayerController : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class PlayerController : MonoBehaviour
     public BoxCollider2D playerCollider;
     public Rigidbody2D rb;
     public Vector2 velocity;
+    Vector2 positionLastFrame;
 
     public Transform groundCheck;
     public float groundDistance = 0.1f;
@@ -28,6 +30,7 @@ public class PlayerController : MonoBehaviour
     public float groundBufferDuration;
     public float stepHeight;
     public float wallJumpSlipTime;
+    public float wallJumpBufferDuration;
     public bool canWallJump;
 
     bool canInterruptJump;
@@ -37,6 +40,7 @@ public class PlayerController : MonoBehaviour
     bool holdingJump;
     bool holding;
     bool slipping;
+    float holdJumpDirection;
 
 
     PlayerMovementDirection playerMovementDirection;
@@ -48,22 +52,7 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         CheckInputs();
-        if (holding == true)
-        {
-            playerObject.GetComponent<SpriteRenderer>().color = Color.red;
-        }
-        if (slipping == true)
-        {
-            playerObject.GetComponent<SpriteRenderer>().color = Color.yellow;
-        }
-        if (slipping == true && holding == true)
-        {
-            playerObject.GetComponent<SpriteRenderer>().color = Color.blue;
-        }
-        if (slipping == false && holding == false)
-        {
-            playerObject.GetComponent<SpriteRenderer>().color = Color.white;
-        }
+        UpdateDebugVisuals();
     }
 
     void CheckInputs()
@@ -91,8 +80,40 @@ public class PlayerController : MonoBehaviour
             holdingJump = false;
         }
     }
+    void UpdateDebugVisuals()
+    {
+        //displays various player-states for debugging
+        if (holding == true)
+        {
+            playerObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        if (slipping == true)
+        {
+            playerObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+        }
+        if (slipping == true && holding == true)
+        {
+            playerObject.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+        if (slipping == false && holding == false)
+        {
+            playerObject.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
     void FixedUpdate()
     {
+        CheckParameterAccessibility();
+        TryStepUp(velocity);
+        TryHold(velocity);
+        velocity = CheckMovement(velocity);
+        velocity = CheckGravity(velocity);
+        if (holding) velocity = Vector2.zero;
+        rb.velocity = velocity;
+        EndParameterAccessibilty();
+    }
+    void CheckParameterAccessibility()
+    {
+        //resets on-ground parameters
         if (IsGrounded(velocity))
         {
             canInterruptJump = true;
@@ -101,6 +122,7 @@ public class PlayerController : MonoBehaviour
             slipping = false;
             velocity.y = -5;
         }
+        //sets walljump/hold/slip parameters
         if (canHoldOnWall == false && holding == false && slipping == false)
         {
             canHoldOnWallTimer += Time.fixedDeltaTime;
@@ -113,21 +135,28 @@ public class PlayerController : MonoBehaviour
         {
             canHoldOnWallTimer = 0;
         }
-        TryStepUp(velocity);
-        TryHold(velocity);
-        velocity = CheckMovement(velocity);
-        velocity = CheckGravity(velocity);
-        if (holding) velocity = Vector2.zero;
-        rb.velocity = velocity;
-        pressedJump = false;
+        //checks head-hitters
+        if (IsTopFree(playerObject.transform.position) == false)
+        {
+            velocity.y = 0;
+        }
+        //updates on-ground buffer for jumping
         enabledGroundBuffer -= Time.fixedDeltaTime;
         if (enabledGroundBuffer < 0) { enabledGroundBuffer = 0; }
         disabledGroundBuffer -= Time.fixedDeltaTime;
         if (disabledGroundBuffer < 0) { disabledGroundBuffer = 0; }
     }
 
+    void EndParameterAccessibilty()
+    {
+        //updates Past-Velocity-Update parameters
+        pressedJump = false;
+        positionLastFrame = playerObject.transform.position;
+    }
+
     Vector2 CheckMovement(Vector2 velocity)
     {
+        //checks jump movement
         if (pressedJump && (IsGrounded(velocity) || holding || slipping))
         {
             pressedJump = false;
@@ -139,9 +168,17 @@ public class PlayerController : MonoBehaviour
             {
                 holding = false;
                 slipping = false;
-                velocity.x = horizontalMaxSpeed * GetPlayerMovementDirectionVector().x * -1;
+                velocity.x = horizontalMaxSpeed * holdJumpDirection;
             }
         }
+
+        //checks Jump interrupt
+        if (holdingJump == false && velocity.y > 0 && canInterruptJump)
+        {
+            velocity.y = 0;
+        }
+
+        //checks walk movement
         if (disabledGroundBuffer <= 0)
         {
             switch (playerMovementDirection)
@@ -167,11 +204,6 @@ public class PlayerController : MonoBehaviour
             if (velocity.x > horizontalMaxSpeed) velocity.x = horizontalMaxSpeed;
             if (velocity.x < -horizontalMaxSpeed) velocity.x = -horizontalMaxSpeed;
         }
-
-        if (holdingJump == false && velocity.y > 0 && canInterruptJump)
-        {
-            velocity.y = 0;
-        }
         return velocity;
     }
 
@@ -186,6 +218,7 @@ public class PlayerController : MonoBehaviour
     }
     void TryStepUp(Vector2 currentVelocity)
     {
+        //check stepup logic
         if (IsGrounded(currentVelocity) == false) { return; }
 
         Vector2 origin = (Vector2)playerRoot.transform.position + Vector2.up * 0.05f;
@@ -202,7 +235,7 @@ public class PlayerController : MonoBehaviour
             case PlayerMovementDirection.None:
                 return;
         }
-
+        //check if player can stepup
         RaycastHit2D lowerHit = Physics2D.Raycast(
             origin,
             direction,
@@ -239,7 +272,7 @@ public class PlayerController : MonoBehaviour
             }
             counter--;
         }
-
+        //player steps up
         if (!upperHit)
         {
             playerObject.transform.position += Vector3.up * (currentStepHeight + stepHeight / 5) + (Vector3)direction * 0.1f;
@@ -247,6 +280,7 @@ public class PlayerController : MonoBehaviour
     }
     void TryHold(Vector2 currentVelocity)
     {
+        //check hold logic
         if (IsGrounded(currentVelocity) == true || disabledGroundBuffer > 0 || canHoldOnWall == false || slipping == true || holding == true) return;
 
         Vector2 origin = (Vector2)playerRoot.transform.position + Vector2.up * 0.05f;
@@ -263,7 +297,13 @@ public class PlayerController : MonoBehaviour
             case PlayerMovementDirection.None:
                 return;
         }
+        if (TryHoldLedge(origin, direction)) return;
+        else TryHoldWall(origin, direction);
+    }
 
+    bool TryHoldLedge(Vector2 origin, Vector2 direction)
+    {
+        //check if player can hold
         bool foundHit = Physics2D.Raycast(
             origin,
             direction,
@@ -271,7 +311,7 @@ public class PlayerController : MonoBehaviour
             groundLayer
         );
         float currentHeight = 0;
-        float maxHeight = playerObject.transform.localScale.y * 1f;
+        float maxHeight = playerObject.transform.localScale.y * 1f - 0.05f;
 
         while (foundHit == false && currentHeight < maxHeight)
         {
@@ -284,7 +324,7 @@ public class PlayerController : MonoBehaviour
             );
         }
 
-        if (foundHit == false) return;
+        if (foundHit == false) return false;
 
         while (foundHit == true && currentHeight < maxHeight)
         {
@@ -296,55 +336,76 @@ public class PlayerController : MonoBehaviour
                 groundLayer
             );
         }
-
-        //checkBotFree
         float targetHeight = (origin + Vector2.up * currentHeight).y - playerObject.transform.localScale.y / 2;
         Vector2 targetPosition = new Vector2(playerObject.transform.position.x, targetHeight);
-        bool downCheck = Physics2D.Raycast(
-            targetPosition,
-            Vector2.down,
-            playerObject.transform.localScale.y / 2,
-            groundLayer
-        );
-        bool downCheckRight = Physics2D.Raycast(
-            targetPosition + Vector2.right * playerObject.transform.localScale.x,
-            Vector2.down,
-            playerObject.transform.localScale.y / 2,
-            groundLayer
-        );
-        bool downCheckLeft = Physics2D.Raycast(
-            targetPosition + Vector2.left * playerObject.transform.localScale.x,
-            Vector2.down,
-            playerObject.transform.localScale.y / 2,
-            groundLayer
-        );
 
-        if (downCheck && downCheckLeft && downCheckRight) return;
+        if (IsBotFree(targetPosition) == false) return false;
 
-        //checkTopFree
-        for (int i = 0; i < 5; i++)
-        {
-            bool topHit = Physics2D.Raycast(
-                (Vector2)playerObject.transform.position + Vector2.right * (-playerObject.transform.localScale.x / 2 + (playerObject.transform.localScale.x * i / 5)),
-                Vector2.up,
-                playerObject.transform.localScale.y / 2 + 0.05f,
-                groundLayer
-                );
-            if (topHit == true)
-            {
-                return;
-            }
-        }
+        if (IsTopFree(playerObject.transform.position) == false) return false;
 
+        //check if player holds on edge or on straight wall
         if (foundHit == false)
         {
-            StartCoroutine(Hold(playerObject, targetPosition, playerMovementDirection, float.MaxValue));
+            StartCoroutine(Hold(playerObject, targetPosition, playerMovementDirection, float.MaxValue, wallJumpBufferDuration));
+            return true;
         }
-        else if (canWallJump)
-        {
-            StartCoroutine(Hold(playerObject, playerObject.transform.position, playerMovementDirection, wallJumpSlipTime));
-        }
+        return false;
     }
+
+    bool TryHoldWall(Vector2 origin, Vector2 direction)
+    {
+        if (canWallJump == false) return false;
+
+        //check if player can hold
+        bool foundHit = Physics2D.Raycast(
+            origin,
+            direction,
+            playerCollider.size.x * playerObject.transform.localScale.x / 2 + 0.05f,
+            groundLayer
+        );
+        float currentHeight = 0;
+        float maxHeight = playerObject.transform.localScale.y * 1f - 0.05f;
+
+        while (foundHit == false && currentHeight < maxHeight)
+        {
+            currentHeight += playerObject.transform.localScale.y / 100;
+            foundHit = Physics2D.Raycast(
+                origin + Vector2.up * currentHeight,
+                direction,
+                playerCollider.size.x * playerObject.transform.localScale.x / 2 + 0.05f,
+                groundLayer
+            );
+        }
+
+        if (foundHit == false) return false;
+
+        while (foundHit == true && currentHeight < maxHeight)
+        {
+            currentHeight += playerObject.transform.localScale.y / 100;
+            foundHit = Physics2D.Raycast(
+                origin + Vector2.up * currentHeight,
+                direction,
+                playerCollider.size.x * playerObject.transform.localScale.x / 2 + 0.5f,
+                groundLayer
+            );
+        }
+        float targetHeight = (origin + Vector2.up * currentHeight).y - playerObject.transform.localScale.y / 2;
+        Vector2 targetPosition = new Vector2(playerObject.transform.position.x, targetHeight);
+
+        if (IsBotFree(targetPosition) == false) return false;
+
+        if (IsTopFree(playerObject.transform.position) == false) return false;
+
+        //check if player holds on edge or on straight wall
+        if (foundHit == false)
+        {
+            return false;
+        }
+
+        StartCoroutine(Hold(playerObject, playerObject.transform.position, playerMovementDirection, wallJumpSlipTime, wallJumpBufferDuration));
+        return true;
+    }
+
     bool IsGrounded(Vector2 velocity)
     {
         RaycastHit2D downCheck = Physics2D.Raycast(
@@ -368,12 +429,48 @@ public class PlayerController : MonoBehaviour
 
         bool grounded = (downCheck || downCheckLeft || downCheckRight);
 
+        //sets a decreasing ground-Buffer for jumping
         if (grounded)
         {
             enabledGroundBuffer = groundBufferDuration;
         }
         return (grounded || (enabledGroundBuffer > 0 && disabledGroundBuffer <= 0));
     }
+    bool IsTopFree(Vector2 targetPosition)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            bool topHit = Physics2D.Raycast(
+                targetPosition + Vector2.right * (-playerObject.transform.localScale.x / 2 + (playerObject.transform.localScale.x * i / 10)),
+                Vector2.up,
+                playerObject.transform.localScale.y / 2 + 0.1f,
+                groundLayer
+                );
+            if (topHit == true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    bool IsBotFree(Vector2 targetPosition)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            bool topHit = Physics2D.Raycast(
+                targetPosition + Vector2.right * (-playerObject.transform.localScale.x / 2 + (playerObject.transform.localScale.x * i / 10)),
+                Vector2.down,
+                playerObject.transform.localScale.y / 2 + 0.05f,
+                groundLayer
+                );
+            if (topHit == true)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
     bool IsTouchingWall()
     {
         for (int i = 0; i < 5; i++)
@@ -411,17 +508,22 @@ public class PlayerController : MonoBehaviour
         playerMovementDirection = PlayerMovementDirection.None;
     }
 
-    IEnumerator Hold(GameObject playerObject, Vector2 targetPosition, PlayerMovementDirection playerMovementDirection, float slipTime)
+    IEnumerator Hold(GameObject playerObject, Vector2 targetPosition, PlayerMovementDirection pMD, float slipTime, float wallJumpBufferDuration)
     {
         holding = true;
         canInterruptJump = true;
         canHoldOnWall = false;
         playerObject.transform.position = targetPosition;
+        holdJumpDirection = pMD == PlayerMovementDirection.Right ? -1 : 1; //inverted to jump away from wall
 
-        if (playerMovementDirection == PlayerMovementDirection.Right)
+        if (pMD == PlayerMovementDirection.Right)
         {
-            while (GameInputManager.GetManagerKey("Right") && (holding || slipping))
+            while ((GameInputManager.GetManagerKey("Right") || wallJumpBufferDuration > 0) && (holding || slipping))
             {
+                if (GameInputManager.GetManagerKey("Right") == false)
+                {
+                    wallJumpBufferDuration -= Time.deltaTime;
+                }
                 if (holding == true)
                 {
                     playerObject.transform.position = targetPosition;
@@ -437,10 +539,14 @@ public class PlayerController : MonoBehaviour
                 yield return null;
             }
         }
-        if (playerMovementDirection == PlayerMovementDirection.Left)
+        else if (pMD == PlayerMovementDirection.Left)
         {
-            while (GameInputManager.GetManagerKey("Left") && (holding || slipping))
+            while ((GameInputManager.GetManagerKey("Left") || wallJumpBufferDuration > 0) && (holding || slipping))
             {
+                if (GameInputManager.GetManagerKey("Left") == false)
+                {
+                    wallJumpBufferDuration -= Time.deltaTime;
+                }
                 if (holding == true)
                 {
                     playerObject.transform.position = targetPosition;
