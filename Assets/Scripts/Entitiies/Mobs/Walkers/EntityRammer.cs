65c73b8f386f9696ad7d5038d38fee349c4ed1e6
JohannesWinter <jvf.winter@web.de>
2026-01-20 01:40:14 +0100

    AddedRammer

--- Assets/Scripts/Entitiies/Mobs/Walkers/EntityRammer.cs
+++ Assets/Scripts/Entitiies/Mobs/Walkers/EntityRammer.cs
@@ -6,6 +6,56 @@ public class EntityRammer : EntityWalker
 {
     public float ramAcceleration;
     public float ramMaxSpeed;
+    public bool ramming;
+    
 
+    protected override void FixedUpdate()
+    {
+        if (SimpleRaycastCheck(currentMovementDirection, 30, entityObject, 1,layerMask: 9)) ramming = true;
+        base.FixedUpdate();
+    }
 
+    protected override void BaseMovement()
+    {
+        if (ramming == false)
+        {
+            base.BaseMovement();
+        }
+        else
+        {
+            if (currentMovementDirection == SimpleEntityMovementDirection.East)
+            {
+                if (velocity.x < ramMaxSpeed)
+                {
+                    velocity.x += 1 * Time.fixedDeltaTime * ramAcceleration;
+                }
+                else if (velocity.x > ramMaxSpeed + 0.1f)
+                {
+                    velocity.x -= 1 * Time.fixedDeltaTime * ramAcceleration;
+                }
+                if (IsGrounded() && (IsAtLedge(currentMovementDirection, distance: walkwayEndReactionSpeed * (ramMaxSpeed/maxSpeed) / acceleration, minLedgeDepth: stepHeight) > 0 || IsTouchingWall(currentMovementDirection, maxDistance: walkwayEndReactionSpeed / acceleration)))
+                {
+                    currentMovementDirection = SimpleEntityMovementDirection.West;
+                    ramming = false;
+                }
+
+            }
+            if (currentMovementDirection == SimpleEntityMovementDirection.West)
+            {
+                if (velocity.x > -ramMaxSpeed)
+                {
+                    velocity.x -= 1 * Time.fixedDeltaTime * ramAcceleration;
+                }
+                else if (velocity.x < -ramMaxSpeed - 0.1f)
+                {
+                    velocity.x += 1 * Time.fixedDeltaTime * ramAcceleration;
+                }
+                if (IsGrounded() && (IsAtLedge(currentMovementDirection, distance: walkwayEndReactionSpeed *  (ramMaxSpeed / maxSpeed) / acceleration, minLedgeDepth: stepHeight) > 0 || IsTouchingWall(currentMovementDirection, maxDistance: walkwayEndReactionSpeed / acceleration)))
+                {
+                    currentMovementDirection = SimpleEntityMovementDirection.East;
+                    ramming = false;
+                }
+            }
+        }
+    }
 }
